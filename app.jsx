4f2e2f84b2bd787311614c3524dee131251efe77
carina120794@gmail.com <you@example.com>
2017-09-27 23:42:30 -0500

    lista pasajeros

--- app.jsx
+++ app.jsx
@@ -1,24 +1,26 @@
 
 
 class Passenger{
-	constructor (nombre,apellido,dni, nPasaje) {
-  this.name = nombre;
-	this.last_name= apellido;
+	constructor (nombre,apellido,dni, asiento) {
+  this.nombre = nombre;
+	this.apellido= apellido;
 	this.dni = dni;
-  this.num = nPasaje; 
-    
+  this.asiento= asiento;
+
   }
 }
 let asientos = new Array(10);
 
-asientos[2] = new Passenger ("Haidy","Dueñas",47797236,3);
-asientos[1] = new Passenger ("Anni","Dueñas",72797236,2);
-asientos[4] = new Passenger ("Gary","Dueñas",34457236,5);
-asientos[6] = new Passenger ("Panchito","Dueñas",54465676,4);
+asientos[8] = new Passenger ("Maria","Salazar",48597236,8);
+asientos[10] = new Passenger ("Sofia","Salazar",7280236,10);
+asientos[4] = new Passenger ("Gladyz","Peña",405087236,4);
+asientos[6] = new Passenger ("Monica","Rodriges",34450676,6);
+asientos[5] = new Passenger ("Javier","Peña",88850676,5);
+
 const App = ({title, seats}) => {
    return (
     <div class="container-fluid">
-		<section id="formulario">
+		<section id="cuestionario">
 			<div class="row" >
 				<div class="col-xs-12 col-md-12 col-lg-12 datos text-center" id="scp-Seating" >
 					<div class="form-group col-xs-12 col-md-5 col-lg-5 datos text-center" >
@@ -38,11 +40,11 @@ const App = ({title, seats}) => {
 					<div class=" col-xs-12 col-md-12 col-lg-12" id="table-space">
 						<table>
 						  	<tr class="seating-tr">
-						        <td class="seating-air btn" id="1">1</td>
-						        <td class="seating-air btn" id="2">2</td>
-						        <td class="seating-air btn" id="3">3</td>
-						        <td class="seating-air btn" id="4">4</td>
-						        <td class="seating-air btn" id="5">5</td>
+						        <td class="w3-button w3-yellow" id="1">1</td>
+						        <td class="w3-button w3-yellow" id="2">2</td>
+						        <td class="w3-button w3-yellow" id="3">3</td>
+						        <td class="w3-button w3-yellow" id="4">4</td>
+						        <td class="w3-button w3-yellow" id="5">5</td>
 						    </tr>
 						    <tr class="seating-tr">
 						        <td class="seating-air btn" id="6">6</td>
@@ -64,21 +66,6 @@ const App = ({title, seats}) => {
 					        <div class ="btn" id ="data-save">Enviar</div>
 					    </form>
 			        </div>
-			        <div class="col-xs-12 col-md-12 col-lg-12 no-visibility" id="search">
-			        <h2>Buscar pasajero</h2>
-	           		<div id="custom-search-input">
-	                    <div class="input-group col-md-12">
-                                <input type="text" class="search-query form-control" placeholder="Search" id="dni-search"/>
-                                <span class="input-group-btn">
-                                    <button class="btn btn-danger" type="button" >
-                                        <span class=" glyphicon glyphicon-search" id="data-search"></span>
-                                    </button>
-                                </span>
-                            </div>
-                            <div class="col-xs-12 col-md-12 col-lg-12" id="space-found"></div>
-                        </div>
-                    </div>
-				</div>
 			</div>
 		</section>
 	</div>
@@ -90,23 +77,16 @@ function imprimirLista(){
     asientos.map((item,index)=>{
       return (
         <div className="col-lg-12">
-          <p>{item.name} {item.last_name}</p>
+          <p>{item.nombre} {item.apellido}</p>
           <p>{item.dni}</p>
-          <p>{item.num}</p>
+          <p>{item.asiento}</p>
         </div>
       )
     })
   )
 }
 
-let seats = [
-   [4, 8, 12, 16, 20, 24, 28, 32],
-   [3, 7, 11, 15, 19, 23, 27, 31],
-   [2, 6, 10, 14, 18, 22, 26, 30],
-   [1, 5, 9, 13, 17, 21, 25, 29]
-]
-ReactDOM.render ( 
-   <App title = {'Hola Mundo!'}  seats = {seats} />, 
+ReactDOM.render (
+   <App title = {'Reserva tu vuelo!'}  seats = {seats} />,
    document.getElementById("root")
-); 
- 
+);
